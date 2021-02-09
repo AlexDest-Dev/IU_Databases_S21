31589b6c1ee8e919317eb8cf0ffd419b084f082d
AlexDest <condor000123@gmail.com>
2021-02-09 20:43:59 +0300

    Added Ex 2

--- week 4/Excersise 2.sql
+++ week 4/Excersise 2.sql
@@ -1,11 +1,11 @@
 -- Excersise 2 --
 -- 1 --
 
-CREATE VIEW short_films AS
+CREATE OR REPLACE VIEW short_films AS
 SELECT * FROM film WHERE length < 50;
 
-CREATE VIEW adult_films as
-SELECT * FROM film WHERE rating = 'NC-17'
+CREATE OR REPLACE VIEW adult_films as
+SELECT * FROM film WHERE rating = 'NC-17';
 
 -- 2 --
 
@@ -16,17 +16,15 @@ IN (SELECT film_id FROM film_category AS rel, category AS cat
 	
 -- 3 --
 
-CREATE OR REPLACE FUNCTION payment_date_calculator()
+CREATE OR REPLACE FUNCTION short_films_refresh()
 RETURNS TRIGGER AS
 $$
 BEGIN
-	NEW.amount = (SELECT rental_duration FROM inventory AS inv, rental AS ren, film WHERE NEW.rental_id = ren.rental_id AND ren.inventory_id = inv.inventory_id AND film.film_id = inv.film_id) * 
-				 (SELECT rental_rate FROM inventory AS inv, rental AS ren, film WHERE NEW.rental_id = ren.rental_id AND ren.inventory_id = inv.inventory_id AND film.film_id = inv.film_id);
-	
-	RETURN NEW;
+	CREATE OR REPLACE VIEW short_films AS
+	SELECT * FROM film WHERE length < 50;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER calculate_payment
-AFTER INSERT ON payment
-EXECUTE PROCEDURE payment_date_calculator();
+CREATE TRIGGER short_films_refresher
+BEFORE INSERT ON payment
+EXECUTE PROCEDURE short_films_refresh();
