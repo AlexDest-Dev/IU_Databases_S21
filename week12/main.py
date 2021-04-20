783ea6aad23154d593dd20d3602dfb43f732e597
AlexDest <condor000123@gmail.com>
2021-04-20 11:04:19 +0300

    Week 12 added

---
+++ week12/main.py
@@ -0,0 +1,7 @@
+import redis
+
+r = redis.Redis(host="redis-12810.c262.us-east-1-3.ec2.cloud.redislabs.com", port="12810",
+                password="ar7b5sfw2P5xJZsIE3P9gQ48lxv9abSj")
+print(r)
+
+
